01d58934983fa9078334668c01f78bbfcd7b56c2
Joshua Sun <tandomset5@gmail.com>
2021-09-09 00:10:44 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Table from './table';
+import Maintable from './table';
+
+
+function App() {
+  return (
+    <>
+      <Maintable />
+    </>
+  );
+}
+
+export default App;
---
+++ src/table.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './style.css'
+
+export function Table() {
+    return(
+     
+    <div class = "flexbox-item flexbox-item-3"> 
+        hellosafdasf
+    </div>
+    )
+}
+
+
+
+export default function Maintable() {
+    return(
+        <>
+        <div class = "flexbox-maincontainer"> 
+            <div class = "flexbox-container"> 
+                <Table />
+                <Table />
+                <Table />
+                <Table />
+                <Table />
+                <Table />
+                <Table />
+                <Table />
+                <Table />
+            </div>
+        </div>
+        </>
+    )
+} 
